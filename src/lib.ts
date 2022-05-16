78b85d6d6d48f729f852c305e98353958fb9136b
Emdadul Haque <rafathaque1997@gmail.com>
2022-05-16 10:39:46 +0600

    Update lib.ts

--- src/lib.ts
+++ src/lib.ts
@@ -8,3 +8,8 @@ export function helloWorld(): string {
   console.log("Hello World");
   return "Hello World";
 }
+
+export function helloWorld2(): string {
+  console.log("Hello World");
+  return "Hello World";
+}
